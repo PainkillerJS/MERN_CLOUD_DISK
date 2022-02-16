8c8113748494f07abbf07db3d49d176b17fd8430
Yuriy <nikorezar@gmail.com>
2022-02-17 00:04:41 +0300

    fix: the warning with redux had repaired

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -8,7 +8,10 @@ export const store = configureStore({
     user: usersSlice.reducer,
     files: filesSlice.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
